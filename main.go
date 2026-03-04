e83a431a87793813f2b88b9bbbf865ea3d7e50f2
deep123845 <deep123845@gmail.com>
2026-03-04 18:08:29 -0500

    added reading and writing config

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/deep123845/blogaggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Config: %+v\n", cfg)
+
+	err = cfg.SetUser("deep")
+	if err != nil {
+		log.Fatalf("error writing config: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Config: %+v\n", cfg)
 }
